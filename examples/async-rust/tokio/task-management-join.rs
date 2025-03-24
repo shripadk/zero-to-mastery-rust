9ce6cc3043a80d9c9ce36c79077b0057ff2c649a
Shripad Krishna <shri@sizzle.run>
2025-03-24 20:18:55 +0530

    fix(docs): async tasks in join example must print

--- examples/async-rust/tokio/task-management-join.rs
+++ examples/async-rust/tokio/task-management-join.rs
@@ -2,19 +2,18 @@ use std::time::Duration;
 
 use tokio::time::sleep;
 
-async fn task1() -> &'static str {
+async fn task1() {
     sleep(Duration::from_secs(3)).await;
-    "Task 1 completed"
+    println!("Task 1 completed")
 }
 
-async fn task2() -> &'static str {
+async fn task2() {
     sleep(Duration::from_secs(1)).await;
-    "Task 2 completed"
+    println!("Task 2 completed")
 }
 
 #[tokio::main]
 async fn main() {
-    let (res1, res2) = tokio::join!(task1(), task2());
-    println!("{}", res1);
-    println!("{}", res2);
+    let _ = tokio::join!(task1(), task2());
+    println!("all tasks completed!");
 }
